5d0224180d7ee105522f378287e2fb58f5d42ea4
Juan Jose Elizalde <Juanjo@MacBook-Air-de-Juanjo.local>
2018-02-09 12:43:37 +0100

    Adding filters to search bar. Prototype page

--- JS/listBox.js
+++ JS/listBox.js
@@ -1,27 +1,71 @@
 const filtros = document.getElementById ("filtros");
 const filtros_listDiv = document.getElementById ("filtros_list");
-const button_filtros = document.getElementsByClassName ("button_filtros")
+const button_filtros = document.getElementsByClassName ("button_showFiltros")[0];
+const button_presup = document.getElementsByClassName ("button_filtros")[0];
+const button_porcen = document.getElementsByClassName ("button_filtros")[1];
+const button_usu = document.getElementsByClassName ("button_filtros")[2];
 
 filtros.addEventListener ("click", () => {
   if (filtros_listDiv.style.display == "none") {
     filtros_listDiv.style.display = "block";
+    button_filtros.textContent = 'Ocultar filtros';
   } else {
       filtros_listDiv.style.display = "none";
-      button_filtros.textContent = 'Ocultar filtros';  // no se cambia el texto del boton
+      button_filtros.textContent = 'Mostrar filtros';
   }
 });
 
+// HOW TO MAKE DISAPPEAR THE DROPDOWN LIST WHEN CLICK OUTSIDE LIST
+// function presupuesto() {
+//   myDropdownDiv = document.getElementById("presupuestoDropdown");
+//   myDropdownDiv.style.display = 'block';
+//     if (myDropdownDiv.style.display = 'block') {
+//       window.onclick = function(event) {
+//       event.target != 'myDropdownDiv';
+//       myDropdownDiv.style.display = 'none';
+//       }
+//     }
+// }
 
-
-function myFunction() {
-    myDropdownDiv = document.getElementById("myDropdown");
+button_presup.addEventListener ("click", () => {
+  myDropdownDiv = document.getElementById("presupuestoDropdown");
+  if (myDropdownDiv.style.display == "none") {
     myDropdownDiv.style.display = 'block';
-}
+  } else {
+    myDropdownDiv.style.display = "none";
+  }
+});
 
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (event.target.matches('.dropdown')) {
-//       if (openDropdown.style.display == 'block') {
-//         openDropdown.style.display = 'none');
-//   }
+button_porcen.addEventListener ("click", () => {
+  myDropdownDiv = document.getElementById("porcentajeDropdown");
+  if (myDropdownDiv.style.display == "none") {
+    myDropdownDiv.style.display = 'block';
+  } else {
+    myDropdownDiv.style.display = "none";
+  }
+});
+
+button_usu.addEventListener ("click", () => {
+  myDropdownDiv = document.getElementById("usufructoDropdown");
+  if (myDropdownDiv.style.display == "none") {
+    myDropdownDiv.style.display = 'block';
+  } else {
+    myDropdownDiv.style.display = "none";
+  }
+});
+
+// function presupuesto() {
+//   myDropdownDiv = document.getElementById("presupuestoDropdown");
+//   myDropdownDiv.style.display = 'block';
+// }
+
+//
+// function porcentaje() {
+//     myDropdownDiv = document.getElementById("porcentajeDropdown");
+//     myDropdownDiv.style.display = 'block';
+// }
+//
+// function usufructo() {
+//     myDropdownDiv = document.getElementById("usufructoDropdown");
+//     myDropdownDiv.style.display = 'block';
 // }

733f13a638ddebb56d7bf875ac2d6cc1dc9ee866
Adrian Elizalde <Adrian@MBP-de-Adrian.home>
2018-03-08 14:34:22 +0100

    interface index

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,37 @@
 // buttons index page using event listeners
 
-const buttonIndex = document.getElementsByClassName("buttonIndex");
-  for (i = 0; i < buttonIndex.length; i++) {
-    if (buttonIndex[0].style.backgroundColor = '#84e184') {
-      buttonIndex[0].addEventListener ('click',() => {
-        buttonIndex[0].style.backgroundColor = '#004d00';
-        buttonIndex[1].style.backgroundColor = '#84e184';
-        buttonIndex[2].style.backgroundColor = '#84e184';
-      })
-    }
-    if (buttonIndex[1].style.backgroundColor = '#84e184') {
-      buttonIndex[1].addEventListener ('click',() => {
-        buttonIndex[1].style.backgroundColor = '#004d00';
-        buttonIndex[0].style.backgroundColor = '#84e184';
-        buttonIndex[2].style.backgroundColor = '#84e184';
-      })
-    }
-    if (buttonIndex[2].style.backgroundColor = '#84e184') {
-      buttonIndex[2].addEventListener ('click',() => {
-        buttonIndex[2].style.backgroundColor = '#004d00';
-        buttonIndex[0].style.backgroundColor = '#84e184';
-        buttonIndex[1].style.backgroundColor = '#84e184';
-      })
-    }
-  }
+// const buttonIndex = document.getElementsByClassName("buttonIndex");
+//   for (i = 0; i < buttonIndex.length; i++) {
+//     if (buttonIndex[0].style.backgroundColor = '#84e184') {
+//       buttonIndex[0].addEventListener ('click',() => {
+//         buttonIndex[0].style.backgroundColor = '#004d00';
+//         buttonIndex[1].style.backgroundColor = '#84e184';
+//         buttonIndex[2].style.backgroundColor = '#84e184';
+//       })
+//     }
+//     if (buttonIndex[1].style.backgroundColor = '#84e184') {
+//       buttonIndex[1].addEventListener ('click',() => {
+//         buttonIndex[1].style.backgroundColor = '#004d00';
+//         buttonIndex[0].style.backgroundColor = '#84e184';
+//         buttonIndex[2].style.backgroundColor = '#84e184';
+//       })
+//     }
+//     if (buttonIndex[2].style.backgroundColor = '#84e184') {
+//       buttonIndex[2].addEventListener ('click',() => {
+//         buttonIndex[2].style.backgroundColor = '#004d00';
+//         buttonIndex[0].style.backgroundColor = '#84e184';
+//         buttonIndex[1].style.backgroundColor = '#84e184';
+//       })
+//     }
+//   }
 
 
 // button index page using jquery
 
 $(document).ready(function () {
     $('.carousel').carousel({
-        interval: 4000
+        interval: 3000
     });
-
     $('.carousel').carousel('cycle');
 });
 
@@ -40,8 +39,8 @@ $(document).ready(function () {
 // const colorOrig = $(".button_div").css({backgroundColor:'#549c3875'});
 
 $(".button_div").on("mouseover", function () {
-    $(this).css({backgroundColor: '#28a428'});
+    $(this).css({backgroundColor: '#549c3875'});
     $(".button_div").on("mouseout", function () {
-        $(this).css({backgroundColor: '#549c3875'});
+        $(this).css({backgroundColor: '#ffffff00'});
     });
 });

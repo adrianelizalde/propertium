6eb8588fad2d744197a1f382579293208026a925
Juan Jose Elizalde <Juanjo@MacBook-Air-de-Juanjo.local>
2018-08-23 13:56:45 +0200

    dgfg

--- JS/jquery.js
+++ JS/jquery.js
@@ -2,9 +2,9 @@
 //   $("#hamburger-button").load("jquery/hamburger.html");
 // });
 
-$(function(){
-  $("#search-container-pisos").load("jquery/menuBuscador.html");
-});
+// $(function(){
+//   $("#search-container-pisos").load("jquery/menuBuscador.html");
+// });
 
 $(function(){
   $("#container-perfil").load("jquery/menuPerfil.html");

f5c250d2d9379b4c96cd89bfa9942702d7cc7459
Juan Jose Elizalde <Juanjo@MacBook-Air-de-Juanjo.local>
2018-08-23 17:58:52 +0200

    sfhfd

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,6 +1,6 @@
-// $(function(){
-//   $("#hamburger-button").load("jquery/hamburger.html");
-// });
+$(function(){
+  $("#hamburger-button").load("jquery/hamburger.html");
+});
 
 // $(function(){
 //   $("#search-container-pisos").load("jquery/menuBuscador.html");
